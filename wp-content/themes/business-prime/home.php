701fdd8c19215e03029780c59c4f2d8cbc2cf5eb
Ghazi Majdoub <majdoub.ghazi@gmail.com>
2017-03-19 09:45:50 -0400

    supprime les themes non-utilises

--- wp-content/themes/business-prime/home.php
+++
@@ -1,7 +0,0 @@
-<?php 
-get_header();
-get_template_part('template-parts/home-slider');
-get_template_part('template-parts/home-services');
-get_template_part('template-parts/home-callout');
-get_template_part('template-parts/home-blog');
-get_footer();

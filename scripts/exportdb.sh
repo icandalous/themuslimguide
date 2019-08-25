a9a9b0d1882d81f6ea9cf821f2dc2075eba80d04
Moussa Thimbo <moussa.thimbo@gmail.com>
2019-08-25 13:40:09 -0400

    import et export db

--- scripts/exportdb.sh
+++ scripts/exportdb.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-mysql --user=homestead --password=secret themslguidedb < ../db/themslguidedb.sql
+mysqldump --user=homestead --password=secret themslguidedb > "../db/themslguidedb.sql"
---
+++ scripts/importdb.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mysql --user=homestead --password=secret themslguidedb < "../db/themslguidedb.sql"
